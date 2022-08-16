f539c3071f2a5b6ec712415df55eb79e3895b45c
clemzouuu <92720413+clemzouuu@users.noreply.github.com>
2022-08-16 15:53:00 +0200

    up to date

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -4,6 +4,7 @@ let url
 
 export default function Meme() {
 
+  // By default, the top and bottom texts are empty and an image displayed on the screen
   const [meme, setMeme] = useState({
     topText: '',
     bottomText: '',
@@ -12,14 +13,17 @@ export default function Meme() {
 
   const [allMemes, setAllMeme] = useState([])
 
+  // At the loading of the page, a fetch is made only once to an API. 
+  // The data that comes back is then used to display a new random image.
   useEffect(() => {
     fetch('https://api.imgflip.com/get_memes')
       .then((res) => res.json())
       .then((data) => setAllMeme(data.data.memes))
+    
   }, [])
 
-  const [currentImage, setNewImage] = useState('images/troll-face.png')
-
+  // When the button is clicked, a random image is displayed on the screen based on the data that cames back from the API which is stored on the allMemes State.
+  // The url of the image is taken from a random number. This url replaces the url from the meme State
   function getMemeImage() {
 
     const randomNumber = Math.floor(Math.random() * allMemes.length)
@@ -31,6 +35,8 @@ export default function Meme() {
     }))
   }
 
+  // This function allows the top and bottom text from the meme State to be equal to what the user is typing.
+  // The state's value is changing everytime the user types characters.
   function handleChange(event) {
     const { name, value } = event.target
     setMeme(prevValue => ({
@@ -38,8 +44,6 @@ export default function Meme() {
       [name] : [value]
     }))
   } 
-
-
 
   return (
     <main id="main">
